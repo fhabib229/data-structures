72de7e75de816d81adcad499372848ebf349d205
Deanlee805 <deanlee805@gmail.com>
2018-08-18 12:40:30 -0700

    V1 stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,22 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function(value, index) {
+    storage[value] = value;
   };
 
   someInstance.pop = function() {
+    var objectLength = Object.keys(storage).length;
+    var lastObj = Object.keys(storage)[objectLength-1];
+    var popElement =  lastObj;
+    // ASK: what's the difference between setting object value = undefined vs delete?
+    // https://stackoverflow.com/questions/208105/how-do-i-remove-a-property-from-a-javascript-object
+    delete storage[lastObj];
+    return popElement;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
