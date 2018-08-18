5d3590bba734e1b2173d460e2620b8a55f64180c
Deanlee805 <deanlee805@gmail.com>
2018-08-18 12:59:47 -0700

    refactor .pop

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -12,11 +12,10 @@ var Stack = function() {
   someInstance.pop = function() {
     var objectLength = Object.keys(storage).length;
     var lastObj = Object.keys(storage)[objectLength-1];
-    var popElement =  lastObj;
     // ASK: what's the difference between setting object value = undefined vs delete?
     // https://stackoverflow.com/questions/208105/how-do-i-remove-a-property-from-a-javascript-object
     delete storage[lastObj];
-    return popElement;
+    return lastObj;
   };
 
   someInstance.size = function() {
