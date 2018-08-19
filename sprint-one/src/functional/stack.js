e3b30ccfe22f88e06d60bceec4f3a532cac3facd
Faris Habib <faris.habib.7@gmail.com>
2018-08-19 15:33:02 -0700

    Stack Passed: allows sequentially additing and removing items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,23 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+
+  };
   var size = 0;
+  var keys = Object.keys(storage);
 
   // Implement the methods below
   someInstance.push = function(value) {
     size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    var garbage = storage[size];
+    delete storage[size];
     size--;
+    return garbage;
   };
 
   someInstance.size = function() {
