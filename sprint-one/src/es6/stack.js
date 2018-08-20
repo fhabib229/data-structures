9a121f006df016df77782671a59ad6e8382fa688
Faris Habib <faris.habib.7@gmail.com>
2018-08-19 21:33:56 -0700

    Completed stack in es6 pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.key = 0;
+    this.storage = {};
+  }
+
+  push(value) {
+    this.key++;
+    this.storage[this.key] = value;
+  }
+
+  pop() {
+    var garbage = this.storage[this.key];
+    delete this.storage[this.key];
+    this.key--;
+    return garbage;
+  }
+
+  size() {
+    if (this.key < 0) {
+      this.key = 0;
+    }
+    return this.key;
   }
 
 }
