b00aac83a9cc13f064f03738ae0c3cfe4b62cc0b
Faris Habib <faris.habib.7@gmail.com>
2018-08-19 21:09:00 -0700

    Completed Stack in prototypal pattern. Fun fact: SpecRunner throws up if you change the name of stackMethods.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storageSize = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+stackMethods = {
+  push: function(value) {
+    this.storageSize++;
+    this.storage[this.storageSize] = value;
+  },
+  pop: function() {
+    var garbage = this.storage[this.storageSize];
+    delete this.storage[this.storageSize];
+    this.storageSize--;
+    return garbage;
+  },
+  size: function() {
+    if (this.storageSize < 0) {
+      this.storageSize = 0;
+    }
+    return this.storageSize;
+  }
+};
 
 
