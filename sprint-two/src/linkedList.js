638583e2d64b8381bae0d0b608c596f11500ad3e
Deanlee805 <deanlee805@gmail.com>
2018-08-26 12:16:06 -0700

    Passed all given test

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,57 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+// {head: {value: 4, next: {value: }}, tail: {value: 5, next: null}}
   list.addToTail = function(value) {
+    // check whether list has a node
+      // if not, initiate node
+      // if list is not empty, find the last node and add a new node to it.
+    var newNode = Node(value);
+    if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      list.tail = newNode;
+      this.head.next = newNode;
+    }
   };
 
   list.removeHead = function() {
+    // Store value of the current head
+    // Then p[oint the current head to the next node's value
+    var formerHeadValue = list.head.value;
+    list.head = this.head.next;
+    return formerHeadValue;
   };
 
   list.contains = function(target) {
+    // Create an array
+    // Iterate through LL until next = null
+      // Push the value properties into the array
+    // If array includes the target
+      // return true
+    var linkedListValue = [];
+    var currentPosition = this.head;
+    while (currentPosition.next !== null) {
+      linkedListValue.push(currentPosition.value);
+      linkedListValue.push(currentPosition.next.value);
+      currentPosition = currentPosition.next;
+    }
+
+    // for every property in linkedlist
+    // iterate through properties
+    // push property value into array
+    // for (var node in list.head) {
+    //   if (node.hasOwnProperty('value')) {
+    //     listedListValue.push(node.value)
+    //   }
+    // }
+
+    if (linkedListValue.includes(target)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return list;
