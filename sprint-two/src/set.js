34891d09899444b4eff9bc317678b66f77e5c28b
Deanlee805 <deanlee805@gmail.com>
2018-08-26 14:42:38 -0700

    passed first 2 tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
+  set.numProp = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.numProp++;
+  this._storage[this.numProp] = item;
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage) {
+    if (this._storage[key] === item) {
+      return true;
+    }
+  }
+  return false;
+
 };
 
 setPrototype.remove = function(item) {
