9ec701b1d38c86fa251439d38f694b2b5bccd174
Deanlee805 <deanlee805@gmail.com>
2018-08-26 10:55:44 -0700

    revert back to original code

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,50 +1,15 @@
 var LinkedList = function() {
   var list = {};
-  // initialize linkedList instance
-  list.linkedListNode = Node();
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
-    // linked list format: {value: 4, next: {value: 5, next: {value: 6, next: null}}}
-
-    // first check if the linkedList is empty
-    if (this.linkedListNode.value === undefined) {
-      // if yes, initilize node, and point both head and tail to the same node.
-      this.linkedListNode = Node(value);
-      var currentPosition = this.linkedListNode;
-      this.head = currentPosition;
-      this.tail = currentPosition;
-      return this.tail;
-    } else {
-      // if not, find the lastNode and add the new node to lastNode.next
-      var currentPosition = this.linkedListNode;
-      while(currentPosition.next !== null) {
-        currentPosition = currentPosition.next;
-      }
-      currentPosition.next = Node(value);
-      this.tail = currentPosition.next;
-      return this.tail;
-    }
   };
 
   list.removeHead = function() {
-    var removedHead = this.head.value;
-    this.linkedListNode = this.linkedListNode.next;
-    this.head = this.head.next;
-    return removedHead;
   };
 
   list.contains = function(target) {
-    var currentPosition = this.linkedListNode;
-    while(currentPosition.value !== target) {
-      currentPosition = currentPosition.next;
-
-      if (currentPosition === null) {
-        return false;
-      }
-    }
-    return true;
   };
 
   return list;
@@ -61,7 +26,4 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- * list.addToTail => O(1);
- * list.removeHead => O(1);
- * list.contains => O(n);
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,7 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = [];  // fix me
-  _.extend(newTree, treeMethods);
+  newTree.children = null;  // fix me
 
   return newTree;
 };
@@ -12,27 +11,12 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  var targetNode = treeNode(value);
-  this.children.push(targetNode);
 };
 
 treeMethods.contains = function(target) {
-  var flattenTree = [];
-  _.each(this.children, function(treeNode){
-    flattenTree.push(treeNode.value);
-  })
-  if (flattenTree.includes(target)) {
-    return true;
-  } else {
-    return false;
-  }
 };
 
-var treeNode = function(value) {
-  var node = [];
-  node.value = value;
-  return node;
-};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
